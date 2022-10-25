f0a7a9cebaaae67af8aa040e2dd4481dc2d80473
maitanegarcia <maitane.garcia02@opendeusto.es>
2022-10-25 12:44:23 +0200

    4 primeros models
    Cine,Sala,Pelicula;Actor
    
    Co-Authored-By: martagorraiz <103249130+martagorraiz@users.noreply.github.com>

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Cine(models.Model):
+    nombre= models.CharField(max_length=50)
+    direccion= models.CharField(max_length=50)
+    telefono= models.CharField(max_length=50)
+    num_salas= models.IntegerField()
+class Sala(models.Model):
+    num_asientos = models.IntegerField()
+    categoria = models.CharField(max_length=50)
+    id_cine = models.ForeignKey(Cine, on_delete=models.)
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=50)
+    fecha_estreno = models.DateField()
+    Longitud_mins = models.FloatField()
+class Actor(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha_nto = models.DateField()

c2b3205cba279d082b50e8efcce772c3fc4c42bf
Iker López <iker.lopezar@opendeusto.es>
2022-10-25 20:00:26 +0200

    urls.py actualizado
    Co-Authored-By: Asier Bujedo Álvarez <90905570+AsierBujedo@users.noreply.github.com>

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/cine' , views.cine, name='cine'),
-    path('/cine/sala' , views.cine, name='salas'),
-    path('/cine/sala/pelicula' , views.cine, name='peliculas'),
-    path('/cine/sala/pelicula/actor' , views.cine, name='actores'),
-    path('/cine/sala/pelicula/director' , views.cine, name='director')
+
+    path('cines' , views.cine, name='main_cines'),
+    path('cine/<int:id_cine>' , views.cine, name='details_cine'),
+
+    path('salas' , views.cine, name='main_salas'),
+    path('salas/<int:id_sala>' , views.cine, name='details_sala'),
+
+    path('peliculas' , views.cine, name='main_peliculas'),
+    path('peliculas/<int:id_pelicula>' , views.cine, name='details_pelicula'),
+    path('peliculas/<int:id_pelicula>/director' , views.cine, name='details_director'),
+    path('peliculas/<int:id_pelicula>/<int:id_actor>' , views.cine, name='details_actor')
 ]

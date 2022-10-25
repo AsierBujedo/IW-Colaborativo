b7d8f185de547e974071f931e2e8a8719b7c7b48
Iker López <iker.lopezar@opendeusto.es>
2022-10-25 19:29:47 +0200

    Adiciones y cambios
    myapp añadido a INSTALLED_APPS de sttings.py.
    Modelos registrados en admin.py.
    algún cambio en models.py.
    Carpeta static creada. Dentro de ella css con main.css y js con main.js.
    Carpeta templates creada. Dentro de ella index.html.

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cine, Sala, Director, Pelicula, Actor 
 
 # Register your models here.
+
+admin.site.register(Cine)
+admin.site.register(Sala)
+admin.site.register(Director)
+admin.site.register(Pelicula)
+admin.site.register(Actor)

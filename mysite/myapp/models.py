508205ad6722097fe1c811b1e87bb26ca01e0e99
martagorraiz <103249130+martagorraiz@users.noreply.github.com>
2022-10-25 13:19:46 +0200

    Update models.py
    Co-Authored-By: maitanegarcia <90905830+maitanegarcia@users.noreply.github.com>

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,20 +1,38 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
 # Create your models here.
 class Cine(models.Model):
+    id_cine = models.IntegerField().primary_key
     nombre= models.CharField(max_length=50)
     direccion= models.CharField(max_length=50)
     telefono= models.CharField(max_length=50)
     num_salas= models.IntegerField()
 class Sala(models.Model):
+    id_sala = models.IntegerField().primary_key
     num_asientos = models.IntegerField()
     categoria = models.CharField(max_length=50)
     id_cine = models.ForeignKey(Cine, on_delete=models.CASCADE)
+class Director(models.model):
+    id_director = models.IntegerField().primary_key
+    nom = models.CharField(max_lenght=50)
+    fecha_nto = models.DateField()
 class Pelicula(models.Model):
+    id_pelicula = models.IntegerField().primary_key
     titulo = models.CharField(max_length=50)
     fecha_estreno = models.DateField()
     Longitud_mins = models.FloatField()
+    id_director = models.ForeignKey(Director, on_delete=models.CASCADE)
 class Actor(models.Model):
+    id_actor = models.IntegerField().primary_key
     nombre = models.CharField(max_length=50)
     fecha_nto = models.DateField()
+class Proyecta(models.model):
+    id_sala = models.IntegerField(Sala, on_delete=models.CASCADE)
+    id_pelicula = models.IntegerField(Pelicula, on_delete=models.CASCADE)
+    fecha = models.DateField().primary_key
+    hora = models.TimeField().primary_key
+class Participa(models.model):
+    id_pelicula = models.IntegerField(Pelicula, on_delete=models.CASCADE)
+    id_actor = models.IntegerField(Actor, on_delete=models.CASCADE)
